ba387c82d95f15626bee79dc21830a5cad212a57
pzinn <pzinnjustin@gmail.com>
2020-04-29 00:11:20 +1000

    update rawhtml

--- src/functions/rawhtml.js
+++ src/functions/rawhtml.js
@@ -2,7 +2,8 @@
 import defineFunction from "../defineFunction";
 import { rawhtml } from "../domTree";
 import mathMLTree from "../mathMLTree";
-// import { calculateSize } from "../units";
+import {assertNodeType} from "../parseNode";
+import { calculateSize } from "../units";
 
 //import * as html from "../buildHTML";
 //import * as mml from "../buildMathML";
@@ -12,19 +13,38 @@ defineFunction({
     names: ["\\rawhtml"],
     props: {
         numArgs: 3,
-	argTypes: ["url","size","size"], // first should be new type ("raw"). FIX
+	//	argTypes: ["raw","size","size"], // unfortunately the new "raw" type is too restrictive (no %)
+	argTypes: ["url","size","size"], // unfortunately the new "raw" type is too restrictive (no %)
         allowedInText: true,
     },
-    handler: (context, args) => {
+    handler: ({parser, funcName, token}, args) => {
+	//        const value = assertNodeType(args[0], "raw").string;
+	const value = args[0].url;
+        if (parser.settings.strict) {
+            parser.settings.reportNonstrict("htmlExtension",
+                "HTML extension is disabled on strict mode");
+        }
+
+        let trustContext = {
+                    command: "\\rawhtml",
+                    content: value
+                };
+
+        if (!parser.settings.isTrusted(trustContext)) {
+            return parser.formatUnsupportedCmd(funcName);
+        }
+
         return {
             type: "rawhtml",
-	    height: args[1].value.number, // eww
-	    depth: args[2].value.number,
-            body: args[0].url,
+	    height: args[1].value,
+	    depth: args[2].value,
+            body: value,
         };
     },
     htmlBuilder: (group, options) => {
-        return new rawhtml(group.height,group.depth,group.body);
+        const height = calculateSize(group.height, options);
+	const depth = calculateSize(group.depth, options);
+        return new rawhtml(height,depth,group.body);
     },
     mathmlBuilder: (group, options) => {// TEMP
         const node = new mathMLTree.MathNode("mspace");
